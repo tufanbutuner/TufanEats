ffa4334c8447e247347a44fcd3a2fe2a1a3a990e
Tufan Butuner <tufan.butuner@transformuk.com>
2023-01-05 11:08:21 +0000

    Displaying items in cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,27 +1,42 @@
+import { useContext } from "react";
+import CartContext from "../../store/CartContext";
 import Modal from "../UI/Modal";
 import "./Cart.css";
+import CartItem from "./CartItem/CartItem";
 
 export default function Cart({ onCloseCart }) {
-  const cartItems = [{ id: "c1", name: "Sushi", amount: 2, price: "12.99" }];
+  const cartCtx = useContext(CartContext);
+
+  const totalAmount = `£${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
+
+  const removeCartItem = (id) => {};
+
+  const addCartItem = (item) => {};
 
   return (
     <Modal onClose={onCloseCart}>
       <ul className="cart-items">
-        {cartItems.map((item) => (
-          <div key={item.id}>
-            <li>{item.name}</li>
-          </div>
+        {cartCtx.items.map((item) => (
+          <CartItem
+            key={item.id}
+            name={item.name}
+            amount={item.amount}
+            price={item.price}
+            onRemove={removeCartItem.bind(null, item.id)}
+            onAdd={addCartItem.bind(null, item)}
+          />
         ))}
       </ul>
       <div className="total">
         <span>Total amount</span>
-        <span>35</span>
+        <span>{totalAmount}</span>
       </div>
       <div className="actions">
         <button className="button--alt" onClick={onCloseCart}>
           Close
         </button>
-        <button className="button">Order</button>
+        {hasItems && <button className="button">Order</button>}
       </div>
     </Modal>
   );
