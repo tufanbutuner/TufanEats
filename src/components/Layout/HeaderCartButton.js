9c977b01d4563344a882f6ad9b4043262174b2eb
Tufan Butuner <tufan.butuner@transformuk.com>
2023-01-05 14:54:14 +0000

    animation on add/remove to cart

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,36 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartContext from "../../store/CartContext";
 import CartIcon from "../Cart/CartIcon";
 import "./HeaderCartButton.css";
 
 export default function HeaderCartButton({ onClick }) {
   const cartCtx = useContext(CartContext);
+  const { items } = cartCtx;
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
 
   const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
 
+  const btnClasses = `button ${btnIsHighlighted ? "bump" : ""}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className="button" onClick={onClick}>
+    <button className={btnClasses} onClick={onClick}>
       <span className="icon">
         <CartIcon />
       </span>
